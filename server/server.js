ce2ba4dffbcf05c8942dd29c165ab59fcbec8066
stockerman7 <stockerman7@naver.com>
2018-02-22 15:12:07 +0900

    Add GET, POST /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log(`3000 포트로 시작되었습니다.`);
 });
