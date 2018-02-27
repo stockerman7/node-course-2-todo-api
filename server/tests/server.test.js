7f3d190909f6a82ed9ff91d62eceb9800b90dbae
stockerman7 <stockerman7@naver.com>
2018-02-27 13:37:50 +0900

    Add Tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 // 모든 데이터 삭제
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,9 +32,9 @@ describe('POST /todos', () => {
         if (err) {
           return done(err); // 비동기 테스트 에러 반환
         }
-
-        Todo.find().then((todos) => {
-          expect(todos.length).toBe(1);
+        // 확인 조회
+        Todo.find({text}).then((todos) => {
+          expect(todos.length).toBe(1); // 찾은 text가 1개인지 확인
           expect(todos[0].text).toBe(text);
           done();
         }).catch((e) => done(e)); // 그럼에도 에러가 발생하면 반환
@@ -43,9 +51,21 @@ describe('POST /todos', () => {
           return done(err);
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('할일을 모두 조회합니다.', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
